a7ce78453f06145bd7f125536a37ec0fd989d9fd
Umesh Chandra Dani <daniumeshchandra.se@gmail.com>
2026-03-26 17:51:39 +0530

    refactor: update PeopleScreen to use ResizablePanel components

--- app/(main)/people/page.tsx
+++ app/(main)/people/page.tsx
@@ -1,3 +1,31 @@
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+
 export default function PeopleScreen() {
-  return <div> Hi, People Page</div>;
+  return (
+    <ResizablePanelGroup direction="horizontal" className="flex-1">
+      <ResizablePanel defaultSize={25}>
+        <div className="h-full">
+          <h1 className="p-3">Friends</h1>
+          <hr />
+          <div className="p-3 flex flex-col justify-between">
+            <div>
+              <div>All</div>
+              <div>Pending</div>
+            </div>
+            <div>
+              <div>Add Friend</div>
+            </div>
+          </div>
+        </div>
+      </ResizablePanel>
+      <ResizableHandle />
+      <ResizablePanel defaultSize={75}>
+        <div className="p-3">Two</div>
+      </ResizablePanel>
+    </ResizablePanelGroup>
+  );
 }
